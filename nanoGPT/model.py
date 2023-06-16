58ae0eeb9ef1f8d9b3c93ce2dab8989b204637da
hgiefers <giefers.heiner@fh-swf.de>
2023-06-16 10:29:07 +0200

    update

--- nanoGPT/model.py
+++ nanoGPT/model.py
@@ -19,6 +19,34 @@ import brevitas.nn as qnn
 #from brevitas.nn import functional as qF
 
 
+################
+from torch import nn
+from typing import Optional
+from brevitas.inject.defaults import Int8ActPerTensorFloat
+from brevitas.inject.defaults import Int8ActPerTensorFloatMinMaxInit
+from brevitas.inject.defaults import Uint8ActPerTensorFloat
+
+from brevitas.nn.quant_layer import ActQuantType
+from brevitas.nn.quant_layer import QuantNonLinearActLayer as QuantNLAL
+
+class QuantGELU(QuantNLAL):
+
+    def __init__(
+            self,
+            act_quant: Optional[ActQuantType] = Uint8ActPerTensorFloat,
+            input_quant: Optional[ActQuantType] = None,
+            return_quant_tensor: bool = False,
+            **kwargs):
+        QuantNLAL.__init__(
+            self,
+            act_impl=nn.GELU,
+            passthrough_act=True,
+            input_quant=input_quant,
+            act_quant=act_quant,
+            return_quant_tensor=return_quant_tensor,
+            **kwargs)
+################
+
 # @torch.jit.script # good to enable when not using torch.compile, disable when using (our default)
 def new_gelu(x):
     """
@@ -38,7 +66,7 @@ class LayerNorm(nn.Module):
     def forward(self, input):
         return F.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)
     
-class CausalSelfAttention_with_MHA(nn.Module):
+class CausalSelfAttention(nn.Module):
 
     def __init__(self, config):
         super().__init__()
@@ -104,66 +132,6 @@ class CausalSelfAttention_with_MHA(nn.Module):
 
 
 
-class CausalSelfAttention(nn.Module):
-
-    def __init__(self, config):
-        super().__init__()
-        assert config.n_embd % config.n_head == 0
-        
-        if config.quantize:
-            # key, query, value projections for all heads, but in a batch
-            self.c_attn = qnn.QuantLinear(config.n_embd, 3 * config.n_embd, bias=config.bias, weight_bit_width=8, bias_bit_width=8)
-            # output projection
-            self.c_proj = qnn.QuantLinear(config.n_embd, config.n_embd, bias=config.bias, weight_bit_width=8, bias_bit_width=8)
-        else:
-            # key, query, value projections for all heads, but in a batch
-            self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
-            # output projection
-            self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
-
-
-        # regularization
-        self.attn_dropout = nn.Dropout(config.dropout)
-        self.resid_dropout = nn.Dropout(config.dropout)
-        self.n_head = config.n_head
-        self.n_embd = config.n_embd
-        self.dropout = config.dropoutq
-        self.quantize = config.quantize
-        # flash attention make GPU go brrrrr but support is only in PyTorch >= 2.0
-        self.flash = hasattr(torch.nn.functional, 'scaled_dot_product_attention')
-        if not self.flash or self.quantize:
-            print("WARNING: using slow attention. Flash Attention requires PyTorch >= 2.0")
-            # causal mask to ensure that attention is only applied to the left in the input sequence
-            self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
-                                        .view(1, 1, config.block_size, config.block_size))
-    
-
-    def forward(self, x):
-        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
-
-        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-        q, k, v  = self.c_attn(x).split(self.n_embd, dim=2)
-        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-
-        # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
-        if self.flash and not self.quantize:
-            # efficient attention using Flash Attention CUDA kernels
-            y = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=True)
-        else:
-            # manual implementation of attention
-            att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-            att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
-            att = F.softmax(att, dim=-1)
-            att = self.attn_dropout(att)
-            y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
-
-        # output projection
-        y = self.resid_dropout(self.c_proj(y))
-        return y
-
 class MLP(nn.Module):
 
     def __init__(self, config):
@@ -172,14 +140,16 @@ class MLP(nn.Module):
         if not self.quantize:
             self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
             self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias)
+            self.gelu     = new_gelu
         else:
             self.c_fc    = qnn.QuantLinear(config.n_embd, 4 * config.n_embd, bias=config.bias, weight_bit_width=8)
             self.c_proj  = qnn.QuantLinear(4 * config.n_embd, config.n_embd, bias=config.bias, weight_bit_width=8)
+            self.gelu    = QuantGELU(bit_width=8)
         self.dropout = nn.Dropout(config.dropout)
 
     def forward(self, x): 
         x = self.c_fc(x)
-        x = new_gelu(x)
+        x = self.gelu(x)
         x = self.c_proj(x)
         x = self.dropout(x)
         return x
@@ -189,8 +159,7 @@ class Block(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.ln_1 = LayerNorm(config.n_embd, bias=config.bias)
-        #self.attn = CausalSelfAttention(config)
-        self.attn = CausalSelfAttention_with_MHA(config)
+        self.attn = CausalSelfAttention(config)
         self.ln_2 = LayerNorm(config.n_embd, bias=config.bias)
         self.mlp = MLP(config)
 
