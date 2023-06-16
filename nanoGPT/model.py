1ab4e6d01ca6784a52939bff41a6aa120633f98d
hgiefers <giefers.heiner@fh-swf.de>
2023-06-16 12:18:24 +0200

    fix broken model

--- nanoGPT/model.py
+++ nanoGPT/model.py
@@ -66,18 +66,13 @@ class LayerNorm(nn.Module):
     def forward(self, input):
         return F.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)
     
-class CausalSelfAttention(nn.Module):
+class CausalSelfAttention_(nn.Module):
 
     def __init__(self, config):
         super().__init__()
         assert config.n_embd % config.n_head == 0       
         
         if config.quantize:
-            # key, query, value projections for all heads, but in a batch
-            self.c_attn = qnn.QuantLinear(config.n_embd, 3 * config.n_embd, bias=config.bias, weight_bit_width=8, bias_bit_width=32)
-            # output projection
-            self.c_proj = qnn.QuantLinear(config.n_embd, config.n_embd, bias=config.bias, weight_bit_width=8, bias_bit_width=32)
-            CausalSelfAttention
             self.mha = qnn.QuantMultiheadAttention(config.n_embd, config.n_head, batch_first=True)
             self.attn_mask = torch.tril(torch.ones((config.block_size,config.block_size)))
 
@@ -105,11 +100,13 @@ class CausalSelfAttention(nn.Module):
     
 
     def forward(self, x):
-        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
-        q, k, v  = self.c_attn(x).split(self.n_embd, dim=2)
+
  
         # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
         if self.flash and not self.quantize:
+
+            B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
+            q, k, v  = self.c_attn(x).split(self.n_embd, dim=2)
             # calculate query, key, values for all heads in batch and move head forward to be the batch dim
             
             k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
@@ -120,15 +117,80 @@ class CausalSelfAttention(nn.Module):
             y = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=True)
 
             y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+            # output projection
+            y = self.resid_dropout(self.c_proj(y))
 
         else:
             # manual implementation of attention
-            y, weights = self.mha(x, x, x, attn_mask=self.attn_mask if self.training else None) # Q, K, V, attn_mask for causality
+            y, weights = self.mha(x, x, x, attn_mask=self.attn_mask if self.training else None)
+            #y = self.resid_dropout(self.c_proj(y))
+            return y
 
+
+
+class CausalSelfAttention(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0       
         
-        # output projection
-        y = self.resid_dropout(self.c_proj(y))
-        return y
+        if config.quantize:
+            self.mha = qnn.QuantMultiheadAttention(config.n_embd, config.n_head, batch_first=True)
+            self.attn_mask = torch.tril(torch.ones((config.block_size,config.block_size)))
+
+        else:
+            # key, query, value projections for all heads, but in a batch
+            self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
+            # output projection
+            self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
+            # flash attention make GPU go brrrrr but support is only in PyTorch >= 2.0
+            self.flash = hasattr(torch.nn.functional, 'scaled_dot_product_attention')
+            if not self.flash:
+                print("WARNING: using slow attention. Flash Attention requires PyTorch >= 2.0")
+                # causal mask to ensure that attention is only applied to the left in the input sequence
+                self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                                            .view(1, 1, config.block_size, config.block_size))
+
+
+        # regularization
+        self.attn_dropout = nn.Dropout(config.dropout)
+        self.resid_dropout = nn.Dropout(config.dropout)
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        self.dropout = config.dropout
+        self.quantize = config.quantize
+       
+
+
+    def forward(self, x):
+        if not self.quantize:
+            B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
+
+            # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+            q, k, v  = self.c_attn(x).split(self.n_embd, dim=2)
+            k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+            q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+            v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+
+            # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
+            if self.flash:
+                # efficient attention using Flash Attention CUDA kernels
+                y = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=True)
+            else:
+                # manual implementation of attention
+                att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+                att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+                att = F.softmax(att, dim=-1)
+                att = self.attn_dropout(att)
+                y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+            y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+
+            # output projection
+            y = self.resid_dropout(self.c_proj(y))
+            return y
+        else:
+            y, weights = self.mha(x, x, x, attn_mask=self.attn_mask if self.training else None) # Q, K, V, attn_mask y
+            return y
 
 
 
