5a1bf31cb85d6b85c82586d3a8144910219c0bf8
hgiefers <giefers.heiner@fh-swf.de>
2023-06-21 13:40:54 +0200

    update

--- nanoGPT/model.py
+++ nanoGPT/model.py
@@ -65,68 +65,6 @@ class LayerNorm(nn.Module):
 
     def forward(self, input):
         return F.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)
-    
-class CausalSelfAttention_(nn.Module):
-
-    def __init__(self, config):
-        super().__init__()
-        assert config.n_embd % config.n_head == 0       
-        
-        if config.quantize:
-            self.mha = qnn.QuantMultiheadAttention(config.n_embd, config.n_head, batch_first=True)
-            self.attn_mask = torch.tril(torch.ones((config.block_size,config.block_size)))
-
-        else:
-            # key, query, value projections for all heads, but in a batch
-            self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
-            # output projection
-            self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
-
-
-        # regularization
-        self.attn_dropout = nn.Dropout(config.dropout)
-        self.resid_dropout = nn.Dropout(config.dropout)
-        self.n_head = config.n_head
-        self.n_embd = config.n_embd
-        self.dropout = config.dropout
-        self.quantize = config.quantize
-        # flash attention make GPU go brrrrr but support is only in PyTorch >= 2.0
-        self.flash = hasattr(torch.nn.functional, 'scaled_dot_product_attention')
-        if not self.flash or self.quantize:
-            print("WARNING: using slow attention. Flash Attention requires PyTorch >= 2.0")
-            # causal mask to ensure that attention is only applied to the left in the input sequence
-            self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
-                                        .view(1, 1, config.block_size, config.block_size))
-    
-
-    def forward(self, x):
-
- 
-        # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
-        if self.flash and not self.quantize:
-
-            B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
-            q, k, v  = self.c_attn(x).split(self.n_embd, dim=2)
-            # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-            
-            k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-            q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-            v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-
-            # efficient attention using Flash Attention CUDA kernels
-            y = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=True)
-
-            y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
-            # output projection
-            y = self.resid_dropout(self.c_proj(y))
-
-        else:
-            # manual implementation of attention
-            y, weights = self.mha(x, x, x, attn_mask=self.attn_mask if self.training else None)
-            #y = self.resid_dropout(self.c_proj(y))
-            return y
-
-
 
 class CausalSelfAttention(nn.Module):
 
@@ -408,7 +346,8 @@ class GPT(nn.Module):
         fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
         use_fused = fused_available and device_type == 'cuda'
         extra_args = dict(fused=True) if use_fused else dict()
-        optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=betas, **extra_args)
+        #optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=betas, **extra_args)
+        optimizer = torch.optim.Adam(optim_groups, lr=learning_rate, betas=betas, **extra_args)
         print(f"using fused AdamW: {use_fused}")
 
         return optimizer
