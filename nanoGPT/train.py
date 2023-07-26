c35d7bb28085b2439a57de2ee96bae31049a2fc2
Max Kuhmichel <kuhmichel.max@fh-swf.de>
2023-07-26 11:59:58 +0000

    RnD

--- nanoGPT/train.py
+++ nanoGPT/train.py
@@ -33,8 +33,8 @@ from model import GPTConfig, GPT
 # default config values designed to train a gpt2 (124M) on OpenWebText
 # I/O
 out_dir = 'out'
-eval_interval = 2000
-log_interval = 1
+eval_interval = 1000
+log_interval = 10
 eval_iters = 200
 eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = True # if True, always save a checkpoint after each eval
@@ -46,7 +46,7 @@ wandb_run_name = 'gpt2' # 'run' + str(time.time())
 # data
 dataset = 'openwebtext'
 gradient_accumulation_steps = 5 * 8 # used to simulate larger batch sizes
-batch_size = 6 # if gradient_accumulation_steps > 1, this is the micro-batch size
+batch_size = 24 # if gradient_accumulation_steps > 1, this is the micro-batch size
 block_size = 1024
 # model
 n_layer = 12
@@ -71,8 +71,8 @@ min_lr = 6e-5 # minimum learning rate, should be ~= learning_rate/10 per Chinchi
 backend = 'nccl' # 'nccl', 'gloo', etc.
 # system
 device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1' etc., or try 'mps' on macbooks
-dtype = 'float16' # 'float32', 'bfloat16', or 'float16', the latter will auto implement a GradScaler, bfloat16 was nanoGPT default?
-compile = False # use PyTorch 2.0 to compile the model to be faster
+dtype = 'bfloat16' # 'float32', 'bfloat16', or 'float16', the latter will auto implement a GradScaler, bfloat16 was nanoGPT default?
+compile = True # use PyTorch 2.0 to compile the model to be faster
 # model components
 _transformer_active_funcs = {
     'LeakyReLU': torch.nn.LeakyReLU,
@@ -142,7 +142,7 @@ def get_batch(split):
     return x, y
 
 # init these up here, can override if init_from='resume' (i.e. from a checkpoint)
-iter_num = 0
+iter_num = 1
 best_val_loss = 1e9
 
 # attempt to derive vocab_size from the dataset
@@ -262,6 +262,7 @@ if wandb_log and master_process:
     wandb.init(project=wandb_project, name=wandb_run_name, config=config)
 
 # training loop
+print("start train")
 X, Y = get_batch('train') # fetch the very first batch
 t0 = time.time()
 local_iter_num = 0 # number of iterations in the lifetime of this process
@@ -277,6 +278,7 @@ while True:
     # evaluate the loss on train/val sets and write checkpoints
     if iter_num % eval_interval == 0 and master_process:
         losses = estimate_loss()
+        print(losses)
         print(f"step {iter_num}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
         if wandb_log:
             wandb.log({
--- nanoGPT/train.sh
+++ nanoGPT/train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # python train.py config/train_shakespeare_char.py --device=cpu --compile=False --eval_iters=20 --log_interval=1 --block_size=64 --batch_size=12 --n_layer=4 --n_head=4 --n_embd=128 --max_iters=2000 --lr_decay_iters=2000 --dropout=0.0
-python train.py config/eval_gpt2.py --device=cuda #--init_from=scratch
+python train.py config/eval_gpt2.py --device=cuda #--init_from=resume
