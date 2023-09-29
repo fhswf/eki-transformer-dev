a871d89c825c1b32b9689f5bb6142a1d982a6f92
BotchkarevPC <botchkarev.maik@fh-swf.de>
2023-09-29 14:06:06 +0200

    added argument passing for classloader

--- qtransform/qtransform/classloader/__init__.py
+++ qtransform/qtransform/classloader/__init__.py
@@ -1,13 +1,13 @@
 from typing import Any
 from types import ModuleType
-from omegaconf import DictConfig
 from logging import Logger
 from qtransform.utils.introspection import get_classes
 
-def get_data(log: Logger, package_name: ModuleType, class_name: str, parent_class: type) -> Any:
+def get_data(log: Logger, package_name: ModuleType, class_name: str, parent_class: type, args = None) -> Any:
     """
         A generic implementation to dynamically load classes from a given package with a given name.
         It is used for dataset, tokenizer and model currently.
+        The returned class is either a wrapper or a model and can be configured with the args parameter.
     """
     #get all classes which are subclasses of DatasetWrapper within own package context
     c = get_classes(package_name, parent_class)
@@ -15,4 +15,8 @@ def get_data(log: Logger, package_name: ModuleType, class_name: str, parent_clas
         log.error(f"{parent_class.__name__} {class_name} not found in {package_name.__name__}")
         raise KeyError
     data_wrapper: Any = c[class_name]
+    #construct an object with given parameters
+    #TODO: consider if this is a good idea
+    if args:
+        return data_wrapper(args)    
     return data_wrapper
--- qtransform/qtransform/model/__init__.py
+++ qtransform/qtransform/model/__init__.py
@@ -8,9 +8,9 @@ def get_model(model_cfg: DictConfig) -> nn.Module:
     """ get model info and return a configured torch nn.Module instance """
     log.debug(f"get_model config: {model_cfg}")
     from qtransform import model as _model
-    model = get_data(log, _model, model_cfg.cls, nn.Module)
-    if "args" in model_cfg:
-        model = model(model_cfg.args)
-    else: 
+    args = model_cfg.get("args")
+    model = get_data(log, package_name = _model, class_name = model_cfg.cls, parent_class = nn.Module, args=args)
+    #construct model if no args have been given
+    if not args:
         model = model()
     return model
