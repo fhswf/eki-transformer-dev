52df89c14a0ab55811adf5855f060c18e70ae604
BotchkarevPC <botchkarev.maik@fh-swf.de>
2023-10-20 14:03:42 +0200

    first design template of brevitas quantization

--- qtransform/qtransform/quantization/brevitas_quant.py
+++ qtransform/qtransform/quantization/brevitas_quant.py
@@ -3,7 +3,33 @@ import brevitas
 from torch.nn import Module
 import logging
 from qtransform.quantization import Quantizer
+from qtransform.classloader import get_data
 from typing import Tuple
+from dataclasses import dataclass
+from omegaconf import DictConfig
+
+#brevitas allows tweaking the quantization hyperparameters for each layer with the parameter weight_quant
+#idea: pass these configs from hydra conf to each layer and override default configs found in brevitas.nn.scale_int
+#the default for linear layers is for example Int8WeightPerTensorFloat (8 bits, int, per tensor, etc.)
+
+@dataclass 
+class ModelQuantConfig():
+    name: str
+    model: DictConfig #key-value pair of each layer within the model along with its quantization parameters
+
+@dataclass
+class WeightConfig():
+    """
+        Configuration to override the attributes from brevitas quantizers (e.g. Int8WeightPerTensorFloat).
+    """
+    bit_width: int
+    signed: bool
+    scaling_per_output_channel: bool #Scope of scale for weights; False: per tensor, True: per channel
+    pass
+
+@dataclass 
+class ActivationConfig():
+    pass
 
 log = logging.getLogger(__package__)
 
@@ -14,9 +40,20 @@ class BrevitasQuantizer(Quantizer):
         quantization as well as allowing quantization on a specified number of bits. 
     """
     def get_quantized_model(self, model: Module) -> Module:
+
+        for module_name, quant_args in ModelQuantConfig().model.items():
+            
+            #
+            new_module = ""
+            model[module_name] = new_module
+
         raise NotImplementedError()
 
     def train_qat(self, model: Module, function: any, args: list) -> Module:
+        """
+            Unlike pytorch, no special function has to be called in order to calibrate the qparams and train the model.
+        """
+        function(model, *args)
         raise NotImplementedError()
 
     def export_model(self, model: Module, filepath: str) -> None:
