62a004f6910a5f90be980932942ef4c773f77952
Max Kuhmichel <kuhmichel.max@fh-swf.de>
2023-10-31 13:47:13 +0100

    make all dirs, not just the last

--- qtransform/qtransform/dataset/tokenizer/__init__.py
+++ qtransform/qtransform/dataset/tokenizer/__init__.py
@@ -8,7 +8,7 @@ from abc import ABC, abstractclassmethod
 from numpy import dtype
 from glob import glob
 from os.path import isdir, exists
-from os import mkdir
+import os
 from numpy import ndarray
 import pickle
 #TODO: maybe implement pytorch.utils.get_tokenizer()
@@ -64,7 +64,7 @@ def get_files(tokenizer_cfg: DictConfig) -> List:
     raw_dir = concat_paths(raw_dir)
     if not exists(raw_dir):
         log.debug(f'Creating directory {raw_dir}')
-        mkdir(raw_dir)
+        os.makedirs(raw_dir)
     log.debug(f'Checking for files with name containing{tokenizer_cfg.name} under directory: {raw_dir}')
     return [x for x in glob(raw_dir + tokenizer_cfg.name + '*') if not isdir(x)]
 
@@ -82,7 +82,7 @@ def save_tokens(ids: ndarray,tokenizer_cfg: DictConfig, meta: Dict = None) -> No
     #directory seperator included in output_dir
     if not exists(output_dir):
         log.debug(f'Creating directory {output_dir}')
-        mkdir(output_dir)
+        os.makedirs(output_dir)
     if meta != None:
         with open(output_dir + filename + '-meta.pkl', 'wb') as f:
             pickle.dump(meta, f)
