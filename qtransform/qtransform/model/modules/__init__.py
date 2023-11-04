dcbb31c06b4c6971d80b7dc2d531408781466a19
Max Kuhmichel <kuhmichel.max@fh-swf.de>
2023-11-04 13:00:57 +0100

    use torch mha, it is more stable

--- qtransform/qtransform/model/modules/__init__.py
+++ qtransform/qtransform/model/modules/__init__.py
@@ -26,7 +26,6 @@ from brevitas.inject.defaults import Uint8ActPerTensorFloat
 
 from brevitas.nn.quant_layer import ActQuantType
 from brevitas.nn.quant_layer import QuantNonLinearActLayer as QuantNLAL
-
 class QuantGELU(QuantNLAL):
     """Does not work so well"""
     def __init__(
@@ -56,74 +55,22 @@ class CausalSelfAttention(nn.Module):
     def __init__(self, config):
         super().__init__()
         assert config.n_embd % config.n_head == 0       
-        
-        if config.quantize:
-            self.mha = qnn.QuantMultiheadAttention(config.n_embd, config.n_head, batch_first=True)
-            self.attn_mask = torch.tril(torch.ones((config.block_size,config.block_size))) # limit to left in the input sequence
-        else: 
-            # key, query, value projections for all heads, but in a batch
-            self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
-            # output projection
-            self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
-            # flash attention make GPU go brrrrr but support is only in PyTorch >= 2.0
-            self.flash = hasattr(torch.nn.functional, 'scaled_dot_product_attention')
-            if not self.flash:
-                print("WARNING: using slow attention. Flash Attention requires PyTorch >= 2.0")
-                # causal mask to ensure that attention is only applied to the left in the input sequence
-                self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
-                                            .view(1, 1, config.block_size, config.block_size))
-
-        # regularization
-        self.attn_dropout = nn.Dropout(config.dropout)
-        self.resid_dropout = nn.Dropout(config.dropout)
         self.n_head = config.n_head
         self.n_embd = config.n_embd
         self.dropout = config.dropout
         self.quantize = config.quantize
-        self.flash = config.flash
         self.block_size = config.block_size
 
+        if config.quantize:
+            self.mha = qnn.QuantMultiheadAttention(config.n_embd, config.n_head, batch_first=True)
+            self.attn_mask = torch.tril(torch.ones((config.block_size,config.block_size))) # limit to left in the input sequence
+        else: 
+            self.mha = nn.MultiheadAttention(config.n_embd, config.n_head, dropout=self.dropout, batch_first=True)
+            self.attn_mask = torch.tril(torch.ones((config.block_size,config.block_size))) # limit to left in the input sequence
+
     def forward(self, x):
-        if self.quantize:
-            y, weights = self.mha(x, x, x, attn_mask=self.attn_mask if self.training else None) # Q, K, V, attn_mask y
-            return y
-        else:
-            B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
-
-            # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-            q, k, v  = self.c_attn(x).split(self.n_embd, dim=2)
-            k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-            q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-            v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-
-            # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
-            if self.flash:
-                # efficient attention using Flash Attention CUDA kernels
-                # "scaled_dot_product_attention" does not export to onnx in all versions of pytorch , presuambly fixed in current nightly builds:
-                #   https://github.com/pytorch/pytorch/issues/97262
-                y = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=True)
-            else:
-                # # manual implementation of attention
-                # att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-                # att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
-                # att = F.softmax(att, dim=-1)
-                # att = self.attn_dropout(att)
-                # y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-
-                def scaled_dot_product_attention(Q,K,V, is_causal:bool, dropout_p:float, source_length, target_length, attn_mask=None):
-                    # Efficient implementation equivalent to the following:
-                    attn_mask = torch.ones(target_length, source_length, dtype=torch.bool).tril(diagonal=0) if is_causal else attn_mask
-                    attn_mask = attn_mask.masked_fill(not attn_mask, -float('inf')) if attn_mask.dtype==torch.bool else attn_mask
-                    attn_weight = torch.softmax((Q @ K.transpose(-2, -1) / math.sqrt(Q.size(-1))) + attn_mask, dim=-1)
-                    attn_weight = torch.dropout(attn_weight, dropout_p)
-                    return attn_weight @ V
-                y  = scaled_dot_product_attention(q,k,v,is_causal=True, dropout_p=self.dropout, source_length=self.block_size, target_length=self.block_size)
-
-            y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
-
-            # output projection
-            y = self.resid_dropout(self.c_proj(y))
-            return y
+        y, weights = self.mha(x, x, x, attn_mask=self.attn_mask if self.training else None) # Q, K, V, attn_mask y
+        return y
         
 class MLP(nn.Module):
     def __init__(self, config):
