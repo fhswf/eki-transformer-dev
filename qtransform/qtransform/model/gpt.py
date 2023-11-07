24ea8f7865d866b90d601d7551716fefc2bc3714
Max Kuhmichel <kuhmichel.max@fh-swf.de>
2023-11-07 09:43:57 +0100

    rename layers

--- qtransform/qtransform/model/gpt.py
+++ qtransform/qtransform/model/gpt.py
@@ -106,15 +106,15 @@ class GPT(nn.Module):
         x = self.transformer.dropout(tok_emb + pos_emb)
         for block in self.transformer.layer:
             x = block(x)
-        x = self.transformer.linear(x)
+        x = self.transformer.ln_out(x)
 
         if targets is not None:
             # if we are given some desired targets also calculate the loss
-            logits = self.lm_out(x)
+            logits = self.linear_out(x)
             loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
         else:
             # inference-time mini-optimization: only forward the lm_head on the very last position
-            logits = self.lm_out(x[:, [-1], :]) # note: using list [-1] to preserve the time dim
+            logits = self.linear_out(x[:, [-1], :]) # note: using list [-1] to preserve the time dim
             loss = None
 
         return logits, loss
