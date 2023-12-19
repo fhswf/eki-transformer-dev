0f5acc108ca313ac00857bcc32b7a9de6fe82a1d
Max Kuhmichel <kuhmichel.max@fh-swf.de>
2023-12-19 15:57:32 +0100

    dirty workaround for missing flash attention in older torch versions

--- qtransform/qtransform/model/modules/__init__.py
+++ qtransform/qtransform/model/modules/__init__.py
@@ -61,11 +61,46 @@ class CausalSelfAttention(nn.Module):
         self.block_size = config.block_size
         self.mha = nn.MultiheadAttention(config.n_embd, config.n_head, dropout=self.dropout, batch_first=True)
         self.attn_mask = torch.nn.parameter.Parameter(torch.tril(torch.ones((config.block_size,config.block_size)))) # limit to left in the input sequence
+        self.flash = config.flash
+        # in case we need to do attention by hand:
+        if (not self.flash) or torch.__version__[3] < 2:
+            log.warn("WARNING: using slow attention. Flash Attention requires PyTorch >= 2.2")
+            # causal mask to ensure that attention is only applied to the left in the input sequence
+            self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                                        .view(1, 1, config.block_size, config.block_size))
+            self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
+            self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
+            self.attn_dropout = nn.Dropout(config.dropout)
+            self.resid_dropout = nn.Dropout(config.dropout)
+        #print(self.flash)
+        #print(torch.__version__[2])
 
     def forward(self, x):
-        #QuantMultiheadAttention does not have is_causal in constructor -> use attention mask instead
-        y, weights = self.mha(x, x, x, attn_mask=self.attn_mask if self.training else None) # Q, K, V, attn_mask y
-        #y, weights = self.mha(x, x, x, is_causal=True) # Q, K, V, attn_mask y
+        # this if block is needed for toprch <2.21 where flash attention onnx export does not work
+        if not type(self.mha).__name__ == "QuantMultiheadAttention" or (not self.flash) or torch.__version__[3] < 2:
+
+            #log.warn("Using slower self attention for non quantized execution if torch does not support it or if flash == False")
+            B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
+            # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+            q, k, v  = self.c_attn(x).split(self.n_embd, dim=2)
+            k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+            q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+            v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+            # manual implementation of attention
+            att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+            att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+            att = F.softmax(att, dim=-1)
+            att = self.attn_dropout(att)
+            y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+            y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+
+            # output projection
+            y = self.resid_dropout(self.c_proj(y))
+        else:
+            #QuantMultiheadAttention does not have is_causal in constructor -> use attention mask instead
+            y, weights = self.mha(x, x, x, attn_mask=self.attn_mask if self.training else None, need_weights=False) # Q, K, V, attn_mask y
+            #y, weights = self.mha(x, x, x, is_causal=True) # Q, K, V, attn_mask y
+           
         return y
         
 from logging import getLogger
