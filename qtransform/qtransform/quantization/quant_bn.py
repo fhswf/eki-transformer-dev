c3f7d7cf665a9a6b506535b8f28c2782308b7802
Botchkarev <botchkarev.maik@fh-swf.de>
2024-01-24 10:53:36 +0000

    experimental merging of batchnorm into quant_batchnorm

--- qtransform/qtransform/quantization/quant_bn.py
+++ qtransform/qtransform/quantization/quant_bn.py
@@ -5,6 +5,7 @@ from brevitas.quant.scaled_int import Int8WeightPerTensorFloat
 from typing import Optional, Union
 from brevitas.quant_tensor import QuantTensor
 from torch import Tensor
+from torch.nn import BatchNorm1d
 #test if a quantized layer can be implemented which basically scales the values along a tensor and adds a bias, thereby simulating batch normalization
 class QuantBatchnorm1d(QuantWBIOL, TorchModule):
     def __init__(
@@ -18,8 +19,9 @@ class QuantBatchnorm1d(QuantWBIOL, TorchModule):
         if not isinstance(num_features, int) or num_features <= 0:
             raise AttributeError()
         #do the same as quantidentity
-        self.weight = torch.ones(num_features)
-        self.bias = torch.zeros(num_features)
+        self.num_features = num_features
+        self.weight = torch.ones(self.num_features)
+        self.bias = torch.zeros(self.num_features)
         QuantWBIOL.__init__(
             self,
             weight_quant=weight_quant,
@@ -34,33 +36,22 @@ class QuantBatchnorm1d(QuantWBIOL, TorchModule):
     
     def inner_forward_impl(self, x: Tensor, quant_weight: Tensor, quant_bias: Optional[Tensor]):
         #inner_forward_impl is apparently the actual forward pass of the layer 
+        #multiply each row of x with one value of weight and add bias
         return x * quant_weight[:,None] + quant_bias[:,None]
 
-from brevitas import nn as qnn
-from brevitas.nn import utils as qutils
-from brevitas.proxy import WeightQuantProxyFromInjector, BiasQuantProxyFromInjector
-
-def merge_bn_mha(layer, bn, output_channel_dim=0):
-    #retrieve learnable parameters from batchnorm (scale + bias)
-    out = qutils.mul_add_from_bn(
-        bn_mean=bn.running_mean,
-        bn_var=bn.running_var,
-        bn_eps=bn.eps,
-        bn_weight=bn.weight.data.clone(),
-        bn_bias=bn.bias.data.clone())
-    mul_factor, add_factor = out #scalar values
-    out_ch_weight_shape = qutils.compute_channel_view_shape(layer.out_proj.weight, output_channel_dim)
-    #only multiply current weights with mul_factor
-    layer.out_proj.weight.data.mul_(mul_factor.view(out_ch_weight_shape))
-    if layer.out_proj.bias is not None:
-        out_ch_bias_shape = qutils.compute_channel_view_shape(layer.out_proj.bias, channel_dim=0)
-        layer.out_proj.bias.data.mul_(mul_factor.view(out_ch_bias_shape))
-        layer.out_proj.bias.data.add_(add_factor.view(out_ch_bias_shape))
-    else:
-        layer.out_proj.bias = nn.Parameter(add_factor)
-    #update quanttensor or something
-    if (hasattr(layer, 'out_proj_weight_quant') and
-            isinstance(layer.out_proj_weight_quant, WeightQuantProxyFromInjector)):
-        layer.out_proj_weight_quant.init_tensor_quant()
-    if (hasattr(layer, 'out_proj_bias_quant') and isinstance(layer.out_proj_bias_quant, BiasQuantProxyFromInjector)):
-        layer.out_proj_bias_quant.init_tensor_quant()
+from brevitas.nn.utils import merge_bn
+def merge_quant_bn(bn: BatchNorm1d, quant_bn: QuantBatchnorm1d = None) -> QuantBatchnorm1d:
+    """
+    Merges a BatchNorm layer into QuantBatchnorm1d. To do that, merge_bn from brevitas.nn.utils is used, updating the
+    weights and biases of QuantBatchnorm1d. The merged QuantBatchnorm1d instance will not be as accurate as the unquantized
+    BatchNorm layer however, with each normalized value having an approximate margin of error of 0.05. 
+    If quant_bn is omited, an instance of QuantBatchnorm1d will be created and returned.
+    """
+    if not isinstance(bn, BatchNorm1d):
+        raise TypeError(f'Cannot merge non-batchnorm layer ({type(bn)}).')
+    if quant_bn is None:
+        quant_bn = QuantBatchnorm1d(bn.num_features)
+    elif quant_bn.num_features != bn.num_features:
+        raise ValueError(f'Property num_features are different for quant_bn ({quant_bn.num_features}) and bn {bn.num_features}')
+    merge_bn(layer=quant_bn, bn=bn)
+    return quant_bn
