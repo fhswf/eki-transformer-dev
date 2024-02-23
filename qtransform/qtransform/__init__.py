858de3f9d0150e4cc016f474e01108da79d678d0
Max Kuhmichel <kuhmichel.max@fh-swf.de>
2024-02-23 09:37:28 +0000

    some extra functions for notebooks

--- qtransform/qtransform/__init__.py
+++ qtransform/qtransform/__init__.py
@@ -1,5 +1,6 @@
 from logging import getLogger
 import os
+from contextlib import contextmanager
 from torch import device, cuda, backends
 log = getLogger(__name__)
 
@@ -11,16 +12,56 @@ def main(cfg):
     from qtransform import  __main__ as mn
     mn.main(cfg)
 
-def notebook_run(args):
+
+def jls_extract_def():
     from hydra import initialize, initialize_config_module, initialize_config_dir, compose
     from omegaconf import OmegaConf
     import qtransform 
-    with initialize_config_dir(version_base=None, config_dir=qtransform.get_module_config_path()):
+    import logging
+    import yaml
+    
+    config_path = qtransform.get_module_config_path()
+    with open(os.path.join(config_path, 'hydra','job_logging', 'custom.yaml'), 'r') as stream:
+        config = yaml.load(stream, Loader=yaml.FullLoader)
+    
+    logging.config.dictConfig(config)
+    logging.getLogger().setLevel(logging.INFO) 
+    return initialize_config_dir, config_path, compose
+
+def notebook_run(args):
+    initialize_config_dir, config_path, compose = jls_extract_def()
+    with initialize_config_dir(version_base=None, config_dir=config_path):
         cfg = compose(config_name="config.yaml", overrides=args)
         print(cfg)
         main(cfg)
 
+@contextmanager
+def with_hyrda(arg):
+    """creates a global cfg variable from hydra run config"""
+    initialize_config_dir, config_path, compose = jls_extract_def()
+    with initialize_config_dir(version_base=None, config_dir=config_path):
+        global cfg
+        old_cfg = cfg
+        cfg = compose(config_name="config.yaml", overrides=arg)
+        log = getLogger(__name__)
+        log.info(f"Hydra compose config is: {cfg}")
+        yield
+        cfg = old_cfg
 
+def with_config(arg):
+    """simiar to with_hyrda but only for singular function definition"""
+    def wrapper_decorator(func):
+        def wrapped_func(*args, **kwargs):
+            initialize_config_dir, config_path, compose = jls_extract_def()
+            with initialize_config_dir(version_base=None, config_dir=config_path):
+                cfg = compose(config_name="config.yaml", overrides=arg)
+                log = getLogger(__name__)
+                log.info(f"Hydra compose config is: {cfg}")
+                func(cfg, *args, **kwargs)
+        return wrapped_func
+    return wrapper_decorator
+
+        
 class DeviceSingleton:
     """
         Boilerplate class which contains the device used for the entire process. The reason why a class is created is in order to monitor when changes
