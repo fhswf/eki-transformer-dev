9bb5ed5208fc65421fccecc6c272d86afb50d78b
Botchkarev <botchkarev.maik@fh-swf.de>
2024-02-27 10:51:49 +0000

    fixed buggy inference in causalselfattention

--- qtransform/qtransform/model/modules/__init__.py
+++ qtransform/qtransform/model/modules/__init__.py
@@ -146,16 +146,19 @@ class CausalSelfAttention(nn.Module):
         self.dropout = config.dropout
         self.block_size = config.block_size
         self.mha = nn.MultiheadAttention(config.n_embd, config.n_head, dropout=self.dropout, batch_first=True)
-        self.attn_mask = torch.nn.parameter.Parameter(torch.tril(torch.ones((config.block_size,config.block_size)))) # limit to left in the input sequence
         self.flash = config.flash
         self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
         self.resid_dropout = nn.Dropout(config.dropout)
+        #since synthesis cannot use is_causal due to bool dtype and brevitas mha does not have is_causal in constructor
+        bias = torch.ones(config.block_size, config.block_size).tril(diagonal=0)
+        #torch.mha uses torch.nn.functional.scaled_dot_product_attention, attention mask is added (plus operation) to attention
+        #meaning: explicitly use negative infinity to prevent adding zero instead of masking that value during softmax
+        bias = bias.masked_fill(bias == 0, float('-inf'))
+        self.register_buffer("bias", bias)
         # in case we need to do attention by hand:
         if (not self.flash) or torch.__version__[3] < 2:
             log.warn("WARNING: using slow attention. Flash Attention requires PyTorch >= 2.2")
             # causal mask to ensure that attention is only applied to the left in the input sequence
-            self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
-                                        .view(1, 1, config.block_size, config.block_size))
             self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
             self.attn_dropout = nn.Dropout(config.dropout)
         #print(self.flash)
@@ -165,29 +168,43 @@ class CausalSelfAttention(nn.Module):
         #print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!HELP ME")
         # this if block is needed for toprch <2.21 where flash attention onnx export does not work
 
-        #if not type(self.mha).__name__ == "QuantMultiheadAttention" and (not self.flash): #or torch.__version__ < (2,21):
-        #
-        #    #log.warn("Using slower self attention for non quantized execution if torch does not support it or if flash == False")
-        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
-        #    # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-        q, k, v  = self.c_attn(x).split(self.n_embd, dim=2)
-        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        #    # manual implementation of attention
-        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
-        att = F.softmax(att, dim=-1)
-        att = self.attn_dropout(att)
-        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
-        #
-        # output projection
-        y = self.resid_dropout(self.c_proj(y))
-        #else:
+        '''if not type(self.mha).__name__ == "QuantMultiheadAttention" and (not self.flash): #or torch.__version__ < (2,21):
+        
+            log.warn("Using slower self attention for non quantized execution if torch does not support it or if flash == False")
+            B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
+            #    # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+            q, k, v  = self.c_attn(x).split(self.n_embd, dim=2)
+            k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+            q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+            v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+            #    # manual implementation of attention
+            att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+            att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+            att = F.softmax(att, dim=-1)
+            att = self.attn_dropout(att)
+            y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+            y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side'''
+        if False:
+            pass
+        else:
             #QuantMultiheadAttention does not have is_causal in constructor -> use attention mask instead
-        #y, weights = self.mha(x, x, x, attn_mask=self.attn_mask if self.training else None, need_weights=False) # Q, K, V, attn_mask y
-        #y = self.resid_dropout(self.c_proj(y))
+            """
+            from scaled dot product attention:
+                L: context, S: embedding dimension
+                L, S = query.size(-2), key.size(-2)
+                scale_factor = 1 / math.sqrt(query.size(-1)) if scale is None else scale
+                attn_bias = torch.zeros(L, S, dtype=query.dtype)
+                if is_causal:
+                    assert attn_mask is None
+                    temp_mask = torch.ones(L, S, dtype=torch.bool).tril(diagonal=0)
+                    attn_bias.masked_fill_(temp_mask.logical_not(), float("-inf"))
+                    attn_bias.to(query.dtype)
+
+            """
+            N, C, L = x.size()
+            #due to inference, input features can be lower than specified max context length which causes problem during attention calculation
+            y, weights = self.mha(x, x, x, attn_mask=self.bias[:C,:C], need_weights=False, is_causal=False) # Q, K, V, attn_mask y
+        y = self.resid_dropout(self.c_proj(y))
             #y, weights = self.mha(x, x, x, is_causal=True) # Q, K, V, attn_mask y
         return y
 from logging import getLogger
