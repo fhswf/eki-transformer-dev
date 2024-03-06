6f07f45888da96d40343f3c51bcdd94547055ef9
Max Kuhmichel <kuhmichel.max@fh-swf.de>
2024-03-06 14:47:28 +0000

    some test with huggingface refenreces

--- scripts/gpt2_wiki-text.sh
+++ scripts/gpt2_wiki-text.sh
@@ -1,9 +1,9 @@
 data="dataset=huggingface dataset.name=wikitext dataset.subset=wikitext-103-raw-v1"
 tokenizer="dataset/tokenizer=tiktoken dataset.tokenizer.encoding=gpt2"
-models=( BENCH_gpt2_ReBNP_gpt2small )
+models=( BENCH_gpt2_ReBNP_gpt2small2 )
 quant_models=( BENCH_4b_gpt2_2 BENCH_4b_gpt2_3 BENCH_8b_gpt2_2 BENCH_8b_gpt2_3 )
 
 for model in ${models[@]}
 do
-    qtransform run=train model=$model run.epochs=20 run.max_iters=500 $data $tokenizer dataset.dataloader.batch_size=64
+    qtransform run=train model=$model run.epochs=10 run.max_iters=500 $data $tokenizer dataset.dataloader.batch_size=32
 done
