4d6ac38631471684ca808464df43ed3717abeb39
Max Kuhmichel <kuhmichel.max@fh-swf.de>
2024-03-06 10:14:23 +0000

    bebch scripts

--- scripts/gpt2_wiki-text.sh
+++ scripts/gpt2_wiki-text.sh
@@ -1,13 +1,9 @@
 data="dataset=huggingface dataset.name=wikitext dataset.subset=wikitext-103-raw-v1"
 tokenizer="dataset/tokenizer=tiktoken dataset.tokenizer.encoding=gpt2"
-models=( BENCH_gpt2_ReBNP_tiny BENCH_gpt2_ReBNP_smaller BENCH_gpt2_ReBNP_small BENCH_gpt2_ReBNP_medium BENCH_gpt2_ReBNP_gpt2small )
+models=( BENCH_gpt2_ReBNP_gpt2small )
 quant_models=( BENCH_4b_gpt2_2 BENCH_4b_gpt2_3 BENCH_8b_gpt2_2 BENCH_8b_gpt2_3 )
 
 for model in ${models[@]}
 do
-    qtransform run=train model=$model run.epochs=10 run.max_iters=10000 $data $tokenizer 
-    for quant in  ${quant_models[@]}
-    do
-        qtransform run=train model=$model run.epochs=10 run.max_iters=10000 $data $tokenizer +export=True quantization=qat quantization/model=$quant
-    done
+    qtransform run=train model=$model run.epochs=20 run.max_iters=500 $data $tokenizer dataset.dataloader.batch_size=64
 done
