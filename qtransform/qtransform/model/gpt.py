bb775c661c9c93cce823ab6f90f88f94f7c3bb64
Max Kuhmichel <kuhmichel.max@fh-swf.de>
2024-03-06 15:42:43 +0100

    test loading some state from pretrained

--- qtransform/qtransform/model/gpt.py
+++ qtransform/qtransform/model/gpt.py
@@ -187,11 +187,9 @@ class GPT(nn.Module):
                 block.attn.bias = block.attn.bias[:,:,:block_size,:block_size]
 
     @classmethod
-    def from_pretrained(cls, model_type, override_args=None):
+    def from_pretrained(cls, model, model_type):
         assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
-        override_args = override_args or {} # default to empty dict
-        # only dropout can be overridden see more notes below
-        assert all(k == 'dropout' for k in override_args)
+
         from transformers import GPT2LMHeadModel
         print("loading weights from pretrained gpt: %s" % model_type)
 
@@ -202,44 +200,80 @@ class GPT(nn.Module):
             'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
             'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
         }[model_type]
-        print("forcing vocab_size=50257, block_size=1024, bias=True")
         config_args['vocab_size'] = 50257 # always 50257 for GPT model checkpoints
         config_args['block_size'] = 1024 # always 1024 for GPT model checkpoints
         config_args['bias'] = True # always True for GPT model checkpoints
-        # we can override the dropout rate, if desired
-        if 'dropout' in override_args:
-            print(f"overriding dropout rate to {override_args['dropout']}")
-            config_args['dropout'] = override_args['dropout']
-        # create a from-scratch initialized minGPT model
-        config = GPTConfig(**config_args)
-        model = GPT(config)
+        print("our model")
+        print(model)
+        config = model.config 
+        print(config)
         sd = model.state_dict()
         sd_keys = sd.keys()
         sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] # discard this mask / buffer, not a param
 
+
+        ### troch model import
+        #import torch
+        #model_t = torch.hub.load('huggingface/transformers', 'modelForCausalLM', 'gpt2')
+        #model_t_sd_keys = model_t.state_dict().keys()
+        #print("torch model")
+        #print(model_t)
+        #print(model_t_sd_keys)
+
         # init a huggingface/transformers model
         model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        print("theirs")
+        print(model_hf)
         sd_hf = model_hf.state_dict()
-
         # copy while ensuring all of the parameters are aligned and match in names and shapes
         sd_keys_hf = sd_hf.keys()
         sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')] # ignore these, just a buffer
         sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')] # same, just the mask (buffer)
+
+        
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.ln_1.weight')] # ignore norm layers
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.ln_1.bias')] # ignore norm layers
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.ln_2.weight')] # ignore norm layers
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.ln_2.bias')] # ignore norm layers
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.ln_f.weight')] # ignore norm layers
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.ln_f.bias')] # ignore norm layers
+
+        # fixed by weight tying anyways?
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('lm_head.weight')] # ignore head layer
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('lm_head.bias')] # ignore head layer
+
         transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
         # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
         # this means that we have to transpose these weights when we import them
-        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+
+        # ignore missing keys
+        # assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+
         for k in sd_keys_hf:
+            # h == layer in our model
+            if k.startswith("transformer.h"):   
+                ak = k.split('.')
+                ak[1] = 'layer'
+                _k = '.'.join(ak)
+            else:
+                _k = k
+
+            print(_k)
+            print(sd[_k].shape)
+            print(k)
+            print(sd_hf[k].shape[::-1])
+
             if any(k.endswith(w) for w in transposed):
                 # special treatment for the Conv1D weights we need to transpose
-                assert sd_hf[k].shape[::-1] == sd[k].shape
+                assert sd_hf[k].shape[::-1] == sd[_k].shape
                 with torch.no_grad():
-                    sd[k].copy_(sd_hf[k].t())
+                    sd[_k].copy_(sd_hf[k].t())
             else:
                 # vanilla copy over the other parameters
-                assert sd_hf[k].shape == sd[k].shape
+                assert sd_hf[k].shape == sd[_k].shape
                 with torch.no_grad():
-                    sd[k].copy_(sd_hf[k])
+                    sd[_k].copy_(sd_hf[k])
+        
 
         return model
 
