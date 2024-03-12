934bf39a28fc9bdf7aac6466365e063af6059166
Max Kuhmichel <kuhmichel.max@fh-swf.de>
2024-03-12 14:35:53 +0000

    add graph plotter stuff

--- scripts/metrics.sh
+++ scripts/metrics.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 FILE=$1
 
-echo $1
+echo "[[runs]]"
+echo "log_file = \"$1\""
 
 # model config
-grep  -E -i -w  'Model config:'  $FILE | cut -d '-' -f 4 | sed -n '0,/config/p' | tr -d ' '
-
+output=$(grep  -E -i -w  'Model config:'  $FILE | cut -d '-' -f 4 | sed -n '0,/config/p' | tr -d ' ')
+echo "config = \"$output\""
 # nuber of params
-grep  -E -i -w  'number of parameters:'  $FILE | awk '{print $7}' 
-
+output=$(grep  -E -i -w  'number of parameters:'  $FILE | awk '{print $7}' | sed -n '0,/M/p')
+echo "params = \"$output\""
 # running loss during training 
-grep  -E -i -w  'qtransform.run.train\]\[INFO\] -   batch'  $FILE | awk '{print $7}' | sed 's/.$//' |  awk '{printf("%f\n",$1 )}'
-
+output=$(grep  -E -i -w  'qtransform.run.train\]\[INFO\] -   batch'  $FILE | awk '{print $7}' | sed 's/.$//' |  awk '{printf(" %f ,",$1 )}')
+echo "train_loss = [ $output ]"
 # number of entries per epoch, can be used to split the above
-sed -n '0,/eval_data/p' $FILE  | sed  -n '/train_data/,/eval/p' | wc -l | xargs bash -c 'echo $(($0 - 2))'
-
+output=$(sed -n '0,/eval_data/p' $FILE  | sed  -n '/train_data/,/eval/p' | wc -l | xargs bash -c 'echo $(($0 - 2))')
+echo "eval_iter = $output"
 # eval loss after epochs
-grep  -E -i -w  'AVERAGE EVAL LOSS FOR EPOCH'  $FILE | awk '{print $10}' | awk '{printf("%f\n",$1 )}'
-
+output=$(grep  -E -i -w  'AVERAGE EVAL LOSS FOR EPOCH'  $FILE | awk '{print $10}' | awk '{printf("%f ,",$1 )}')
+echo "eval_loss = [ $output ]"
 # last loss before eval (loss after evry epoch)
-grep  -E -i -w  'last train loss was'  $FILE | awk '{print $8}' |  awk '{printf("%f\n",$1 )}'
-
+output=$(grep  -E -i -w  'last train loss was'  $FILE | awk '{print $8}' |  awk '{printf(" %f ,",$1 )}')
+echo "eval_loss_last_train = [ $output ]"
 # checkpoints paths
-grep  -E -i -w  'Model checkpoint saved'  $FILE | awk '{print $8}'
-
+output=$(grep  -E -i -w  'Model checkpoint saved'  $FILE | awk '{print "\042"$8"\042" ", "}')
+echo "checkpoints = [ $output ]"
 # exported model, might nnot reutrn anything if model was not exported
-grep -E -i  -w 'exporting...'  $FILE | awk -F "exporting..." '{print $2}'sociated | tr -d ' '
-
+output=$(grep -E -i  -w 'exporting...'  $FILE | awk -F "exporting..." '{print $2}'sociated | tr -d ' ')
+echo "export = \"$output\""
 # all the logs
 # find . -name 'log.txt' | xargs  -I % ~/eki/eki-transformer-dev/scripts/metrics.sh %
