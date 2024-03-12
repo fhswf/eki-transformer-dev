637cbf24c51bd7547d56d8ef481896ea64fe3055
Max Kuhmichel <kuhmichel.max@fh-swf.de>
2024-03-12 11:52:27 +0000

    extract script

--- scripts/metrics.sh
+++ scripts/metrics.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-FILE=log.txt
+FILE=$1
+
+echo $1
+
+# model config
+grep  -E -i -w  'Model config:'  $FILE | cut -d '-' -f 4 | sed -n '0,/config/p' | tr -d ' '
+
+# nuber of params
+grep  -E -i -w  'number of parameters:'  $FILE | awk '{print $7}' 
 
 # running loss during training 
 grep  -E -i -w  'qtransform.run.train\]\[INFO\] -   batch'  $FILE | awk '{print $7}' | sed 's/.$//' |  awk '{printf("%f\n",$1 )}'
@@ -16,11 +24,8 @@ grep  -E -i -w  'last train loss was'  $FILE | awk '{print $8}' |  awk '{printf(
 # checkpoints paths
 grep  -E -i -w  'Model checkpoint saved'  $FILE | awk '{print $8}'
 
-# nuber of params
-grep  -E -i -w  'number of parameters:'  $FILE | awk '{print $7}'
-
-# model config
-grep  -E -i -w  'Model config:'  $FILE | cut -d '-' -f 4
-
 # exported model, might nnot reutrn anything if model was not exported
 grep -E -i  -w 'exporting...'  $FILE | awk -F "exporting..." '{print $2}'sociated | tr -d ' '
+
+# all the logs
+# find . -name 'log.txt' | xargs  -I % ~/eki/eki-transformer-dev/scripts/metrics.sh %
