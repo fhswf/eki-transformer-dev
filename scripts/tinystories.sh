3e950f120de32aa0c02feb854be4ec0704a61bd5
Max Kuhmichel <kuhmichel.max@fh-swf.de>
2024-03-19 14:56:25 +0000

    conf cleaning

---
+++ scripts/tinystories.sh
@@ -0,0 +1,10 @@
+data="dataset=lhf_tinystories dataset.dataloader.batch_size=32"
+run="run=train run.epochs=1 "
+quant="quantization=qat quantization/model=BENCH_4b_gpt2"
+models=( BENCH_gpt2_ReBNT_tiny BENCH_gpt2_ReBNT_tiny )
+
+for model in ${models[@]}
+do
+    echo qtransform $run model=$model $data
+    qtransform $run model=$model $data
+done
