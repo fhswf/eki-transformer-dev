d120abb4fd3289b597771f508167f40e116f4b08
Max Kuhmichel <kuhmichel.max@fh-swf.de>
2024-03-20 14:30:56 +0000

    wip

--- scripts/tinystories_quant8b.sh
+++ scripts/tinystories_quant8b.sh
@@ -1,5 +1,6 @@
 data="dataset=lhf_tinystories dataset.dataloader.batch_size=32"
 run="run=train run.epochs=1 "
+quant="quantization=qat quantization/model=BENCH_8b_gpt2"
 models=( BENCH_gpt2_ReBNT_tiny BENCH_gpt2_ReBNT_tiny )
 
 for model in ${models[@]}
