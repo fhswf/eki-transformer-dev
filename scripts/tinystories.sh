66f6284c05eef58faf1c9b9e19eef239172d43f9
Max Kuhmichel <kuhmichel.max@fh-swf.de>
2024-03-22 16:33:23 +0000

    wip

--- scripts/tinystories.sh
+++ scripts/tinystories.sh
@@ -1,6 +1,6 @@
 data="dataset=lhf_tinystories dataset.dataloader.batch_size=32"
 run="run=train run.epochs=1 "
-models=( BENCH_gpt2_ReBNT_tiny BENCH_gpt2_ReBNT_tiny )
+models=( BENCH_gpt2_ReBNP_small BENCH_gpt2_ReBNP_smaller BENCH_gpt2_ReBNP_tiny BENCH_gpt2_ReBNT_small BENCH_gpt2_ReBNT_smaller BENCH_gpt2_ReBNT_tiny BENCH_gpt2_ReLN_small BENCH_gpt2_ReLN_smaller BENCH_gpt2_ReLN_tiny )
 
 for model in ${models[@]}
 do
