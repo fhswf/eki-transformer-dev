1d517be03af649cded0e9a5e307bc6b6564b060d
Max Kuhmichel <kuhmichel.max@fh-swf.de>
2024-05-25 10:13:09 +0200

    tokenizers

--- qtransform/qtransform/scripts/clean_dataset.py
+++ qtransform/qtransform/scripts/clean_dataset.py
@@ -7,7 +7,8 @@ import json
 import glob
 import os
 from typing import Any
-
+from tqdm import tqdm
+from tqdm.contrib.logging import logging_redirect_tqdm
 import enchant
 import qtransform
 args = [ "run=script", "run.file=none" ]
@@ -15,28 +16,27 @@ args = [ "run=script", "run.file=none" ]
 def run_standalone(cfg):
 	log.info(f"running {__file__}")
 	file = "/home/kuhmichel/.qtransform/datasets/downloads/TinyStories_all_data"
-	stats = {
-		"errors": 0,
-		"succcess": 0,
-	}
+	stats = { "errors": 0, "succcess": 0, }
 	ouputs = write_outputs(file="outputs")
 	next(ouputs)
+	with logging_redirect_tqdm():
+		for _i, e in tqdm(enumerate(gather_files(file))):
+			# print(e[Keys.story])
+			if kget(e,Keys.source) == Keys.gpt3:
+				continue
+			good_grammer, data = process_story_grammer(e)
+			good_vocab, data = check_story_vocab(e) 
+			yes = good_grammer and good_vocab
+			if yes:
+				stats['succcess'] = stats['succcess'] + 1
+				ouputs.send(data[Keys.story]+"\n<|endoftext|>\n\n")
+			else:
+				stats['errors'] = stats['errors'] + 1
+				
+		#if _i > 10:
+	#		break
+	ouputs.close()
 
-	for _i, e in enumerate(gather_files(file)):
-		if kget(e,Keys.source) == Keys.gpt3:
-			continue
-		good_grammer, data = process_story_grammer(e)
-		good_vocab, data = process_story_vocab(e) 
-		yes = good_grammer and good_vocab
-		if yes:
-			stats['succcess'] = stats['succcess'] + 1
-			ouputs.send(data[Keys.story])
-			ouputs.send("\n<|endoftext|>\n")
-		else:
-			stats['errors'] = stats['errors'] + 1
-			
-		if _i > 60000:
-			break
 	print(unknown_words)
 	print(stats)
 
@@ -52,23 +52,30 @@ class Keys(str, enum.Enum):
 	gpt3 = "GPT-3.5"
 	def __str__(self):
 		return f'{str(self.value)}'
-	
+		
 #pat = re.compile(r"(.+?)[\s'\",!,\.,\?,-]{1,}")
 pat = re.compile(r"([\w][\w]*'?\w?)")
 number = re.compile(r"^[+-]?((\d+(\.\d+)?)|(\.\d+))$")
+puncts = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '.',
+           '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', 
+           '`', '{', '|', '}', '~', '»', '«', '“', '”']
+punct_pattern = re.compile("[" + re.escape("".join(puncts)) + "]{1}")
+eigenNames = re.compile(r"([A-Z]{1}[\w]+)") 
 unknown_words = dict()
 #english = enchant.Dict("en_US")
 english_us = enchant.DictWithPWL("en_US","mywords.txt")
 english_gb = enchant.DictWithPWL("en_GB","mywords.txt")
 blacklist  = enchant.PyPWL("blacklist.txt")
-def process_story_vocab(data:str):
+def check_story_vocab(data:str):
 	story = data[Keys.story]
 	story_words = pat.findall(story)
 	word:str
 	for word in story_words:
-		if bool(number.search(word)):
+		if bool(number.search(word)): # skip numbers
 			continue
-		if not english_us.check(word) and not english_gb.check(word):
+		if blacklist.check(word):
+			return False, data
+		if not english_us.check(word) and not english_gb.check(word): # try and recover in case we can
 			word = word.rstrip("'r")
 			word = word.rstrip("'l")
 			word = word.rstrip("'s")
@@ -76,17 +83,34 @@ def process_story_vocab(data:str):
 			#word = word.rstrip("'ll")
 			word = word.rstrip("'v")
 			#word = word.rstrip("'ve")
-			if word == "":
-				continue
-			if not english_us.check(word) and not english_gb.check(word):
-				if unknown_words.get(word, None) is not None:
-					unknown_words[word] = unknown_words[word]+1
-				else:
-					unknown_words[word] = 1
+			if word == "": # empty after strip => always missformed word
+				#print("? " + word)
 				return False, data
-		if blacklist.check(word):
-			return False, data
+			if blacklist.check(word): # blacklist again
+				return False, data
+			if english_us.check(word) or english_gb.check(word):  # words after strip
+				continue
+			if english_us.check(word.lower()) or english_gb.check(word.lower()): # kaptial words
+				continue
+			if bool(eigenNames.search(word)): # eigen namen, Fifi, Momo, Juila, Romeo, ...
+				continue
+			_word = word.rstrip("y") # tippy, stormy, tipsy, shacky, etc...
+			if english_us.check(_word) or english_gb.check(_word):  # words after strip
+				continue
+			if english_us.check(_word.lower()) or english_gb.check(_word.lower()): # kaptial words
+				continue
+			_word = word.rstrip("ly") # tippy, stormy, tipsy, shacky, etc...
+			if english_us.check(_word) or english_gb.check(_word):  # words after strip
+				continue
+			if english_us.check(_word.lower()) or english_gb.check(_word.lower()): # kaptial words
+				continue
 
+			if unknown_words.get(word, None) is not None:
+				unknown_words[word] = unknown_words[word]+1
+			else:
+				unknown_words[word] = 1
+			return False, data
+		
 	return True, data
 
 def process_story_grammer(data:dict):
@@ -113,7 +137,7 @@ def gather_files(file):
 			log.debug(f"assume file {file} is a folder")
 			log.debug(f"processing all files in folder")
 			# for file in ["TinyStories_all_data/data00.json"]:
-			for file in glob.glob(os.path.join(file, "*")):
+			for file in sorted(glob.glob(os.path.join(file, "*"))):
 				yield from gather_files(file)
 		elif os.path.isfile(file):
 			log.debug(f"assume file {file} is a pointer to a file")
@@ -161,7 +185,8 @@ def kset(d, key:str, v:Any):
 		log.error(f"d must be of type dict, found {type(d)}")
 		raise Exception 
 
-def write_outputs(file, split_size_bytes = 10000000, overwrite=False):
+# 80000000 = 80MB
+def write_outputs(file, split_size_bytes = 80000000, overwrite=False):
 	""" Generator type writing pipline. used via gen.send(data). Calls str() around data."""
 	if isinstance(file, str):
 		if os.path.isdir(file):
@@ -170,30 +195,46 @@ def write_outputs(file, split_size_bytes = 10000000, overwrite=False):
 			counter = 1
 			# create folder/test001.txt files
 			partfile = os.path.join(file, "part" + str(counter) + ".txt")
+			if os.path.isfile(partfile):
+				if not overwrite:
+					log.error(f"output file present, overwrite: {overwrite}, stopping")
+					raise Exception("File already present")
+				else:
+					log.warning(f"output file present, overwrite: {overwrite}, deleting old file")
+					os.remove(partfile)
 			while True:
 				data = yield
 				if data == None:
 					continue
-				# TODO
+				
 				with open(partfile, 'a') as o:
 					o.write(str(data))
 				if os.path.getsize(partfile) > split_size_bytes:
 					counter = counter + 1
 					partfile = os.path.join(file, "part" + str(counter) + ".txt")
+					if os.path.isfile(partfile):
+						if not overwrite:
+							log.error(f"output file present, overwrite: {overwrite}, stopping")
+							raise Exception("File already present")
+						else:
+							log.warning(f"output file present, overwrite: {overwrite}, deleting old file")
+							os.remove(partfile)
+			#print("exit generator")
 		elif os.path.isdir(os.path.dirname(file)):
 			log.debug(f"assume file {file} is a pointer to a file")
 			if os.path.isfile(file) and not overwrite:
 				log.error(f"output file present, overwrite: {overwrite}, stopping")
-				raise Exception
+				raise Exception("File already present")
 			else:
 				log.warning("Overwriting file")
 			with open(file, 'w') as o:
 				while True:
 					data = yield
 					o.write(str(data))
+			#print("exit generator")
 		else:
 			log.error(f"unsupported filetype {file}, of type {type(file)}")
-			raise Exception 
+			raise Exception(f"unsupported filetype {file}")
 
 def clean_punctuation(s:str) -> None:
 	s = s.strip()
--- qtransform/qtransform/scripts/test.py
+++ qtransform/qtransform/scripts/test.py
@@ -1,107 +1,35 @@
 
+# from datasets import load_dataset
+# dataset = load_dataset("roneneldan/TinyStories")
+# print(dataset)
+# print(dataset['train'][0])
+# 
+# import pyarrow.parquet as pq
+a = "/home/kuhmichel/.qtransform/datasets/huggingface/roneneldan/TinyStories/cache-EleutherAI/gpt-neo-125M-128-EVAL-grouped.arrow"
+b = "/home/kuhmichel/Downloads/0000(1).parquet"
+c = "hf://datasets/roneneldan/TinyStories@691b0d9bd48ade766778c940011ca1c549f6359b/data/train-00000-of-00004-2d5a1467fff1081b.parquet"
+d = "/home/kuhmichel/.cache/huggingface/datasets/roneneldan___tiny_stories/default-ca0b42f82bf0eeae/0.0.0/0111277fb19b16f696664cde7f0cb90f833dec72db2cc73cfdf87e697f78fe02/tiny_stories-train-00000-of-00004.arrow"
+# print(pq.read_metadata(b))
+# table = pq.read_table(b)
+# print(table)
+# df = table.to_pandas()
+# print(df.describe())
+# 
+# for e in df["text"]:
+#     print(e)
+#     break
 
+"".splitlines
+from datasets import Dataset
+ds = Dataset.from_parquet(b)
+print(ds)
+print(ds['text'][0])
+ds = Dataset.from_file(d)
+print(ds)
+print(ds['text'][0])
 
-
-
-
-
-
-# this freezes my computer. let's load 1 at a time instead
-# data = list()
-# for file in glob.glob("TinyStories_all_data/*"):
-# 	with open(file) as o:
-# 		# print(type(json.load(o)))
-# 		data.extend(json.load(o))
-
-thrown_out = 0
-def process_story(par):
-	global thrown_out
-	par = par.strip()
-	par = par.replace('\\', '')
-	par = par.replace('  ', ' ')
-	par = par.replace('–', '-')
-	par = par.replace(' — ', ' - ')
-	par = par.replace('—', ' - ')
-	par = par.replace('…', '...')
-	par = par.replace('“', '"')
-	par = par.replace('”', '"')
-	par = par.replace('’', '\'') # curly quotes. don't replace the opposite side: we want to cull them
-	# in the future, we might try replacing 160 and 180. nah, it doesn't matter. we're only throwing out 1282 paragraphs
-	# there's some grammar mistakes, like missing commas, and ?". which is double punctuation
-	# "\ ", a space after a \
-	for c in par:
-		if ord(c) != 10 and (ord(c) > 127 or ord(c) < 32):
-			thrown_out += 1
-			# print("offending char:", ord(c), c)
-			return ""
-
-	for c in par:
-		if c == '|' or c == '<' or c == '/' or c == '`' or c == '*' or c == '=' or c == '_' or c == '&' or c == '@' or c == '~' or c == '#' or c == '%' or c == '[' or c == ']' or c == '+' or c == '(' or c == ')':
-			# ` is usually as strange punctuation, or `` ''
-			# / is for Tom/Lily stories, "he/she"
-			# * is usually in *emphasis*, but often incorrect, or in *** separating parts of a story
-			# $ is usually correct
-			# & is as an abbrev, or &rsquo;s
-			# ~ is rare and not used well
-			# # is wrong, hashtags and rarely as numbers
-			# there's one mistake for % and the rest are ok. I decided to clean it anyway
-			# [ is used wrong
-			# _ is used poorly
-			# = is sometimes used for addition but more often for mistakes
-			# + is for addition, abbreviation, and A+. but has some mistakes
-			# ( ) is about 80% correctly used
-			thrown_out += 1
-			# print("offending char:", ord(c), c, ":", par) #<i>, <End of Story>, <|im_start|>
-			return ""
-
-	if len(par) < 100:
-		thrown_out += 1
-		# if len(par) > 0:
-		# 	print("too short:", par) # many empty stories or story fragments. some legitimate stories are deleted below 200
-		return ""
-	if par[-1] != '.' and par[-1] != '!' and par[-1] != '"' and par[-1] != '?':
-		thrown_out += 1
-		# print("offending char:", par[-1])
-		# print(par)
-		return ""
-
-	return par
-
-total_data = []
-def clean(d):
-	global total_data
-	# print("generators:", set([i['source'] for i in d])) # it's only GPT-3.5 and GPT-4. no mistakes here
-	d = [process_story(i['story']) for i in d if i['source'] == 'GPT-4']
-	d = [i for i in d if i != ''] # remove empty lines
-	total_data += d
-
-# for file in ["TinyStories_all_data/data00.json"]:
-for file in glob.glob("TinyStories_all_data/*"):
-	with open(file) as o:
-		# print(type(json.load(o)))
-		data = json.load(o)
-		clean(data)
-
-print("thrown out:", thrown_out)
-print("survived:", len(total_data))
-
-# print("uniques", len(set(total_data)))
-# print("duplicates", [item for item, count in collections.Counter(total_data).items() if count > 1]) # no duplicates
-
-# after some removals: Counter({' ': 426859449, 'e': 204912889, 'a': 150822107, 't': 139517558, 'o': 112349865, 'h': 105934005, 'n': 100530370, 'd': 95421243, 'i': 94879636, 's': 84200324, 'r': 75921863, 'l': 67910605, 'y': 51200735, 'm': 44606524, '.': 44303626, 'w': 43890889, 'u': 41336733, 'p': 33429250, 'g': 31785986, 'c': 30360697, 'b': 26834430, 'f': 24930515, ',': 23967379, 'k': 20172791, 'T': 20115680, '"': 12313202, 'v': 11770115, '\n': 9128807, 'S': 8571627, 'H': 6240920, 'I': 5084215, 'O': 4663978, "'": 4138731, 'L': 4092379, '!': 3870671, 'B': 3417017, 'x': 3397591, 'M': 3275768, 'A': 2511284, 'W': 1795287, 'j': 1644565, '?': 1400451, 'Y': 1313533, 'z': 1015289, 'J': 906218, 'F': 825037, 'D': 770337, 'C': 609437, 'q': 596899, 'N': 483349, 'E': 465002, 'K': 337323, 'P': 301036, 'G': 252938, 'R': 226506, '-': 193971, ':': 119893, 'Z': 67185, 'V': 42153, 'U': 31080, '3': 24147, ';': 11440, 'Q': 9834, 'X': 3447, '1': 3416, '0': 2392, '2': 1941, '5': 1672, '4': 770, '/': 472, '9': 433, '`': 366, '8': 356, '6': 317, '7': 273, ')': 199, '(': 194, '$': 155, '_': 102, '*': 88, '&': 44, '=': 43, '+': 27, '[': 18, ']': 18, '%': 13, '#': 9, '~': 5, '@': 2})
-# c = Counter()
-# for s in total_data:
-# 	c += Counter(s)
-# print(c)
-
-# all_chars = set()
-# for i in t:
-# 	all_chars = all_chars.union(set(i))
-# print("all chars", all_chars)
-# all chars {'ó', '\xad', '\t', '❤', '¡', '(', '€', 'r', 'V', '把', 'T', '自', '\u3000', 'Z', '在', '恩', '\u2005', ';', '"', 'ā', '‘', '\u202a', 'p', 'â', '\u2009', '。', ')', '應', '了', '會', 'S', 'h', 'D', 'u', '兒', '剛', '5', 'd', '\ue000', 'İ', '/', 'ᴇ', '9', '¢', '’', '‑', 'è', '當', '\uf04a', 'É', '給', 'N', 'j', '米', '_', '\\', '…', '~', '‐', '―', '獨', '\u200e', 'ᴜ', '度', 'n', 'P', '�', 'a', 'O', 'ñ', '—', 'f', '🎓', '>', 'l', 'Y', 'ᴀ', 's', '\xa0', 'í', '\x92', 'G', '童', 'ғ', 'c', 'E', '7', '£', 'W', '兩', '\u2028', '!', '很', '興', '−', '留', 'á', 'K', 'ᴢ', 'F', '8', '們', '保', 'ʏ', '´', 'J', '但', '又', 'I', '奮', '$', '🤩', '4', '過', '她', '\u200c', '+', 'L', 'o', '3', '是', '🌴', '️', '個', '=', '0', 'b', 'ᴅ', 'q', '己', '·', '天', '#', 'ᴏ', 'A', '1', '他', '*', '莉', '─', '艾', 'R', 'ᴡ', '}', '難', 'U', 'X', '[', '和', '™', '»', ' ', '裡', 'y', 'à', ']', 'C', '?', '些', '整', '¿', '6', ':', '🍌', 'ᴄ', 'ᴛ', 'x', 'ɪ', 'm', 'g', '田', 'e', 'M', '„', ',', '\u200a', '°', '<', '\u2029', '{', '.', '”', '`', 'ö', '的', 'ß', "'", 'Q', 'ú', '%', '分', 'v', '–', 'і', 'ê', 'B', '“', '\ufeff', 'ʙ', 'i', 'z', '«', '@', '-', '一', 'ï', 't', 'w', '巴', '到', '玉', 'k', 'œ', '，', '‚', 'H', 'é', 'ʜ', '&', '§', '2', '\u200b', '這', '答', '高', '|', '時'}
-# print("all chars used:", set(''.join(t))) # maybe this caused my computer to crash?
-
-print(random.choice(total_data))
-
-with open("cleaned_data.pkl", "wb") as file:
-	pickle.dump(total_data, file)
+# import pyarrow as pa
+# with pa.memory_map(d, 'r') as source:
+#     loaded_arrays = pa.ipc.open_file(source).read_all()
+# arr = loaded_arrays[0]
+# print(f"{arr[0]} .. {arr[-1]}")
