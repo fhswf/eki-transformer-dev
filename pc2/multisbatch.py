f76f1d36d3cefd900575c8eea2f2e4a567447453
Max Kuhmichel <kuhmichel.max@fh-swf.de>
2025-09-17 14:19:21 +0200

    update pc2 runs script

--- pc2/multisbatch.py
+++ pc2/multisbatch.py
@@ -4,11 +4,11 @@ import os
 from itertools import product
 import pathlib
 
-static_args = ["run=train run.epochs=1 +model.type=CHECKPOINT quantization=qat run.export=True \
-    debug=True +trace=True +run.max_iters=3000 \
-    wandb.init.project=qtransform \
-    " 
-]
+static_args = "run=train run.epochs=4 +model.type=CHECKPOINT \
+debug=True +trace=True \
+wandb.init.project=qtransform-longer5h \
+" 
+# run.export=True +run.max_iters=5000 \
 
 # done in slurm specific batch script
 # work_env = ["dataset.root_path=$WORK_HOME/.qtransform/datasets"]
@@ -18,22 +18,31 @@ datasets = [
 ]
 
 hyperparam_combinations =  [
-    "dataset.dataloader.batch_size=32", "dataset.dataloader.batch_size=8"
+    "dataset.dataloader.batch_size=32", 
+    # "dataset.dataloader.batch_size=8"
 ]
 
 models = [
     "model=NEW_BENCH2",    
 ]
 model_config = [
-    "model.args.n_layer=2 model.args.n_head=4 model.args.n_embd=256 model.args.block_size=256"
+    "model.args.n_layer=2 model.args.n_head=4 model.args.n_embd=256 model.args.block_size=256",
+    "model.args.n_layer=3 model.args.n_head=4 model.args.n_embd=256 model.args.block_size=256",
+    "model.args.n_layer=2 model.args.n_head=4 model.args.n_embd=512 model.args.block_size=256",
+    "model.args.n_layer=3 model.args.n_head=4 model.args.n_embd=512 model.args.block_size=256",
 ]
 
 model_args_dropout=[
-    "model.args.dropout=0.0", "model.args.dropout=0.1", "model.args.dropout=0.2"
+    "model.args.dropout=0.0", 
+    #"model.args.dropout=0.1", 
+    #"model.args.dropout=0.2"
 ]
 
 model_args_norm_layer=[
-    "model.args.norm_layer=BatchNormTranspose", "model.args.norm_layer=LayerNorm", "model.args.norm_layer=BatchNormIdPure"
+    "model.args.norm_layer=None",
+    #"model.args.norm_layer=BatchNormTranspose", 
+    #"model.args.norm_layer=LayerNorm", 
+    #"model.args.norm_layer=BatchNormIdPure"
 ]
 
 model_args_pos_layer=[
@@ -41,17 +50,17 @@ model_args_pos_layer=[
 ]
 
 optim = [
-    "optim.args.weight_decay=0.0",
-    "optim.args.weight_decay=0.1",
+    #"optim.args.weight_decay=0.0",
+    #"optim.args.weight_decay=0.1",
     "optim.args.weight_decay=0.2",    
 ]
 
 quant = [
     "quantization=qat quantization/model=SLURM_BENCH2",
-    #"quantization=qat quantization/model=SLURM_BENCH3",
+    "quantization=qat quantization/model=SLURM_BENCH3",
     "quantization=qat quantization/model=SLURM_BENCH4",
     #"quantization=qat quantization/model=SLURM_BENCH5",
-    "quantization=qat quantization/model=SLURM_BENCH6",
+    #"quantization=qat quantization/model=SLURM_BENCH6",
     #"quantization=qat quantization/model=SLURM_BENCH7",
     #"quantization=qat quantization/model=SLURM_BENCH8"
     "",
@@ -61,17 +70,17 @@ argument_combinations = list(product(datasets, hyperparam_combinations, models, 
 for args in argument_combinations:
     split_args = []
     for group in args:
-        split_args.extend(group.split())
-
+        split_args.extend(group.split(" "))
+    # split_args = list(args)
     call_args = \
         ["sbatch"] \
         + ["--export=WANDB_API_KEY="+os.environ["wandbkey"]] \
         + [ os.path.join(os.path.dirname(os.path.abspath(__file__)), "apps/generic_run.sh")] \
         + split_args \
-        + static_args
+        + static_args.split(" ")
         #+ work_env
     result = subprocess.run(call_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     
     print("call: " + str(call_args))
     print("stdout: " + str(result.stdout))
-    print("stderr: " + str(result.stderr))
+    #print("stderr: " + str(result.stderr))
